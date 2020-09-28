4a0b6951a8b423a3562e50ec3f1b390d6762eb51
shazy89 <erdo.shazy123@gmail.com>
2020-09-28 12:03:19 -0400

    Done.

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+class Home extends React.Component {
+  render() {
+    return (
+         <div>
+            <h1>Home!</h1>
+            <h4>This is the home page!!!</h4>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Emblem-extra-cool.svg/1200px-Emblem-extra-cool.svg.png" alt="picture" />
+         </div> 
+         )
+  }
+}
+ 
+export default Home;
