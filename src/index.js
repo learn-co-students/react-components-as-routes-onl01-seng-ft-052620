f5194c23dbb6db25fa6da7f6770b4f49f880176c
chrisb3546 <chrisb3546@yahoo.com>
2020-09-30 11:27:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -8,8 +9,9 @@ const Home = () => {
     </div>
   );
 };
-
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>),
   document.getElementById('root')
 );
