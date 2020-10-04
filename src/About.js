6b0c6c81e69719d129871c3b2fdd4c2963194b03
LetsJessCode <letsjesscode@gmail.com>
2020-10-04 12:07:51 -0400

    Done.

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    render() {
+        return (
+            <h3>About Me Section</h3>
+        )
+    }
+}
+
+export default About;
