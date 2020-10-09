82a0ca41f9bb1ba27d98392d4173a4369b22712b
Kristin239 <hkristin@andrew.cmu.edu>
2020-10-09 18:39:58 -0400

    Done.

---
+++ src/Navbar.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+ 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+ 
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavLink
+          to="/"
+          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+          exact
+          /* add styling to Navlink */
+          style={link}
+          /* add prop for activeStyle */
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Home</NavLink>
+        <NavLink
+          to="/about"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >About</NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Login</NavLink>
+      </div>
+    )
+  }
+}
+ 
+export default Navbar;
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
+ReactDOM.render((
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>),
+  document.getElementById('root')
+);
